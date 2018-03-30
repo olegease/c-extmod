964a0fd259cd907cd32149f17d057a2d88d5ec09
Oleg Kharchuk <olegease@gmail.com>
2018-03-30 18:01:55 +0300

    EXTMOD: add system.h and core.h files

---
+++ extmod/core.h
@@ -0,0 +1,6 @@
+#ifndef _EXTMOD_CORE_H_
+#define _EXTMOD_CORE_H_
+
+typedef void* void_;
+
+#endif // _EXTMOD_CORE_H_
---
+++ extmod/system.h
@@ -0,0 +1,6 @@
+#ifndef _EXTMOD_SYSTEM_H_
+#define _EXTMOD_SYSTEM_H_
+
+#include "core.h"
+
+#endif // _EXTMOD_SYSTEM_H_
